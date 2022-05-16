dc6f3c43ceb42e7d7575736ea6e54538f2299bd6
Kirk McAllister <kirk.mcallister1994@gmail.com>
2022-05-16 11:36:20 +0100

    Adding initial multiple buckets functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,18 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double areaOfWall = 0;
         double totalAreaOfWalls = 0;
-        double areaToRemove = 0;
         double totalAreaToRemove = 0;
         double areaToPaint = 0;
         double sizeOfBucket = 0;
         double[] sizeOfBucketsAry;
         int numBuckets = 0;
-        int[] numBucketsAry;
+        int[][] numBucketsAry;
         double numBucketsExact = 0;
 
         Scanner sc = new Scanner(System.in);
@@ -24,7 +23,8 @@ public class Main {
 
         System.out.print("\n");
 
-        boolean running = true;
+        //Adding Areas to paint -- allows adding of multiple areas.
+        boolean running;
         do {
             System.out.println("Please enter the full area of wall, in meters squared:");
 
@@ -33,36 +33,62 @@ public class Main {
             System.out.println("More walls to add? Y/N");
             String input = sc.next();
 
-            running = input.equals("N") ? false : true;
+            running = !input.equals("N");
         } while (running);
 
         //TODO Allow adding more object to remove -- Complete
         //TODO Validate input
 
-        System.out.print("\n\n");
+        System.out.print("\n");
 
-        running = true;
+        //Removing Areas to paint -- allows removing of multiple areas.
         do {
             System.out.println("Please enter any area you would like to remove (e.g. walls, windows, etc), " +
-                    "in meters squared: ");
+                    "in meters squared:");
 
             totalAreaToRemove += sc.nextDouble();
 
             System.out.println("More area to remove? Y/N");
             String input = sc.next();
 
-            running = input.equals("N") ? false : true;
+            running = !input.equals("N");
         } while (running);
 
+        //Calculates Total Area to paint.
         areaToPaint = totalAreaOfWalls - totalAreaToRemove;
 
-        System.out.println("\nPlease enter size of bucket, in litres: ");
-        sizeOfBucket = sc.nextDouble();
+//        System.out.println("\nPlease enter size of bucket, in litres:");
+//        sizeOfBucket = sc.nextDouble();
         //TODO Allow more bucket sizes
         //TODO Validate input
 
-        System.out.print("\n\n");
+        System.out.println("\nPlease enter number of bucket sizes would like to add:");
+        numBucketsAry = new int[sc.nextInt()][2];
 
+        System.out.println("Please enter sizes, in litres (separate each size with a space):");
+        String[] sizes = sc.next().split(" ");
+
+        for (int i = 0; i < sizes.length; i++) {
+            numBucketsAry[i][0] = Integer.parseInt(sizes[i]);
+            numBucketsAry[i][1] = 0;
+        }
+
+        double litresNeeded = areaToPaint / 6.5;
+
+        int index = 0;
+        while (litresNeeded > 0) {
+            if (litresNeeded > numBucketsAry[index][0]) {
+                numBucketsAry[index][1] = (int) Math.ceil(litresNeeded / numBucketsAry[index][0]);
+                litresNeeded %= numBucketsAry[index][0];
+            } else {
+
+            }
+            index++;
+        }
+
+        System.out.print("\n");
+
+        //Calculates the number of buckets of paint required.
         numBucketsExact = areaToPaint / (6.5 * sizeOfBucket);
         numBuckets = (int) Math.ceil(numBucketsExact);
 
